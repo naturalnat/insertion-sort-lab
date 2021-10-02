a009da463c795bf9a7baedc27e9181bedcc3e473
Natalie Taktachev <ntaktach@gmail.com>
2021-10-02 15:06:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+    let minIndex = array.indexOf(Math.min(...array)) //indexOf returns first index at which a given element can be found (in this case: min value in array)
+    return array.splice(minIndex, 1) //removes one element at given index 
 }
 
-function selectionSort(array){
+function selectionSort(array) {
+    let sortedArr = [];
 
+    while (array.length) {
+        let min = findMinAndRemove(array); // min = arr w/o min value 
+        sortedArr.push(...min) //pushing arr w/o min val into new array (sortedArr) 
+    }
+    return sortedArr
 }
